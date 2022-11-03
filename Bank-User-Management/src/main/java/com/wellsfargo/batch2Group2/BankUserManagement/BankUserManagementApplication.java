5db78bcc84f62ba19754dc5bf84d8920f22b1499
Ahmed1423 <mak181354@gmail.com>
2022-11-03 17:37:08 +0530

    Initializing Spring Boot Project

---
+++ Bank-User-Management/src/main/java/com/wellsfargo/batch2Group2/BankUserManagement/BankUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.batch2Group2.BankUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankUserManagementApplication.class, args);
+	}
+
+}
---
+++ Bank-User-Management/src/test/java/com/wellsfargo/batch2Group2/BankUserManagement/BankUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.batch2Group2.BankUserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
