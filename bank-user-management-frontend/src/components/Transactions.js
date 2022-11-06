e546be7f3b9e716907bd54f6d9ee59bd803d554b
nwtnscmndr <nwtnscmndr@gmail.com>
2022-11-06 19:33:08 +0530

    added fields

--- bank-user-management-frontend/src/components/Transactions.js
+++ bank-user-management-frontend/src/components/Transactions.js
@@ -1,7 +1,72 @@
 import React from "react";
 
+const handleSubmit = () => {
+
+};
+
 const Transactions = () => {
-    return <div>Transactions</div>;
+    return <>
+        <div className="mb-md-1 mt-md-1 pb-1">
+                <h2 className="fw-bold text-uppercase">Transactions</h2>
+                <p className="text-dark-50 mb-3">Please the details!</p>
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="form-outline mb-2">
+                        <label
+                          className="form-label"
+                          htmlFor="customer-first-name"
+                        >
+                          Customer ID 
+                        </label>
+                        <input
+                          type="text"
+                          value="CID"
+                          disabled
+                        />
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="form-outline mb-2">
+                        <label
+                          className="form-label"
+                          htmlFor="customer-first-name"
+                        >
+                          Select the Transaction
+                        </label>
+                        <input
+                          type="text"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="form-outline mb-2">
+                        <label
+                          className="form-label"
+                          htmlFor="customer-first-name"
+                        >
+                          Enter the Amount 
+                        </label>
+                        <input
+                          type="number"
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    
+
+
+                  <div className="d-grid gap-2">
+                    <button className="btn btn-info" type="submit">
+                      Apply
+                    </button>
+                  </div>
+                </div>
+                </form>
+              </div>
+    </>;
 };
 
 export default Transactions;
--- bank-user-management-frontend/src/components/ViewStatement.js
+++ bank-user-management-frontend/src/components/ViewStatement.js
@@ -1,7 +1,93 @@
 import React from "react";
 
+const handleSubmit = () => {
+
+};
+
 const ViewStatement = () => {
-    return <div>Statements</div>;
+    return (
+    <>
+        <div className="mb-md-1 mt-md-1 pb-1">
+                <h2 className="fw-bold text-uppercase">View Statements</h2>
+                <p className="text-dark-50 mb-3">Please the details!</p>
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="form-outline mb-2">
+                        <label
+                          className="form-label"
+                          htmlFor="customer-first-name"
+                        >
+                          Customer ID 
+                        </label>
+                        <input
+                          type="text"
+                          value="CID"
+                          disabled
+                        />
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="form-outline mb-2">
+                        <label
+                          className="form-label"
+                          htmlFor="customer-first-name"
+                        >
+                          Select the Transaction
+                        </label>
+                        <input
+                          type="text"
+                          required
+                        />
+                      </div>
+                    </div>
+                    
+                    <div className="col-6">
+                      <div className="form-outline">
+                        <label className="form-label" htmlFor="customer-dob">
+                          From Date
+                        </label>
+                        <input
+                          type="date"
+                          id="customer-dob"
+                          className="form-control form-control-lg"
+                          name="customer-dob"
+                          min="1995-01-01"
+                          max={new Date().toLocaleDateString("en-ca")}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                  <div className="col-6">
+                      <div className="form-outline">
+                        <label className="form-label" htmlFor="customer-dob">
+                          To date
+                        </label>
+                        <input
+                          type="date"
+                          id="customer-dob"
+                          className="form-control form-control-lg"
+                          name="customer-dob"
+                          min="1995-01-01"
+                          max={new Date().toLocaleDateString("en-ca")}
+                          required
+                        />
+                      </div>
+                    </div>
+                    
+
+
+                  <div className="d-grid gap-2">
+                    <button className="btn btn-info" type="submit">
+                      Apply
+                    </button>
+                  </div>
+                </div>
+                </form>
+              </div>
+    </>
+    )
 };
 
 export default ViewStatement;
