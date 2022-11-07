caf6c9cca04d92de01f2126409ab8f75b30260bc
unknown <dhanushsridhar26@gmail.com>
2022-11-07 21:49:40 +0530

    FE - MENU STYLE

--- bank-user-management-frontend/src/components/Menu.js
+++ bank-user-management-frontend/src/components/Menu.js
@@ -1,30 +1,35 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
 import GoToLogin from "./GoToLogin";
 
 const Menu = (props) => {
 
-    const navigate = useNavigate();
-
-    const route_ = (path) => {
-        navigate(path);
-    }
-
-    if(sessionStorage.getItem('user') === null) {
+    if (sessionStorage.getItem('user') === null) {
         return (<GoToLogin />);
-      }
+    }
 
     return (
         <>
-            <h2> Hi, {sessionStorage.getItem('user')}.</h2>
-            <button className="btn btn-info" onClick={() => {route_("/loan")}}>Loan</button>
-            <br></br>
-            <button className="btn btn-info" onClick={() => {route_("/transactions")}}>Transactions</button>
-            <br></br>
-            <button className="btn btn-info" onClick={() => {route_("/viewstatement")}}>View Statement</button>
-            <br></br>
-            <br></br>
-            <button className="btn btn-info" onClick={() => {sessionStorage.removeItem('user'); route_("/login")}}>Logout</button>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Hi, {sessionStorage.getItem('user')}.</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/loan">Apply for Loan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/transactions">Make Transaction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/viewstatement">View Statement</a>
+        </li>    
+      </ul>
+    </div>
+  </div>
+</nav>
         </>
     )
 };
